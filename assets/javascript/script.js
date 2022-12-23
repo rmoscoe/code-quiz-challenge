0094bbb8c1b7722153aad2024f5ebe77a96f37ea
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2022-12-23 15:10:06 -0800

    add initial HTML structure

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
+//Use a class to construct question objects in order to avoid repeating all the inner HTML.
 //Use an array to store question objects. When the Start button is clicked and each time a question is submitted, use a random number to select another question from the array, then remove that question from the array.
-//Each question object contains properties for the question and answer choices.
+//Each question object contains properties for the question and answer choices, as well as inner HTML that includes class attributes.
 //Dynamically create and add elements using the stored properties of the question objects.
-//For the timer, use minute and second intervals that decrement every minute and every second.
+//For the timer, use minute and second variables. Update the display every second and calculate the variables based on a single time remaining variable that decrements every second.
 //For the scores, remember localStorage.setItem("Key", "Value") and localStorage.getItem("Key"). preventDefault() the Submit button for the initials associated with the score.
