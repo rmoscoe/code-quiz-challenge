786b7c3cbccee58185b32288f0e09164e6b5ef33
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2022-12-27 15:56:56 -0800

    improve answer choice display and selection behavior

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,22 +5,19 @@ const mainSec = document.querySelector("main");
 const welcome = document.getElementById("welcome");
 const startButton = document.getElementById("start");
 const rawQuestions = ["Which of the following tags runs JavaScript on a webpage?"];
+const multiSelectQuestion = [false];
 const rawCode = [""];
 const rawAnswers = [[{text:"&ltlink&gt", dataCorrect:"incorrect"}, {text:"&lta&gt", dataCorrect:"incorrect"}, {text:"&ltsrc&gt", dataCorrect:"correct"}, {text:"&lthref&gt", dataCorrect:"incorrect"}]];
 let questions = [];
+let multiSelect = false;
 
 class Question {
-    constructor(id, question, code, answers) {
+    constructor(id, question, multiSelect, code, answers) {
         this.id = id;
         this.question = question;
+        this.multiSelect = multiSelect;
         this.code = code;
         this.answers = answers;
-
-        // if (prevId >= 0) {
-        //     let prevSection = document.getElementById(prevQuestion);
-        //     prevSection.style.display = "none";
-        // }
-
         this.section = document.createElement("section");
         this.section.className = "question";
         this.section.innerHTML = "<h3>" + this.question + "</h3> <br /> <code>" + this.code + "</code><br /> <ul class = 'answers'><li class = 'answer-choice' data-correct = '" + this.answers[0].dataCorrect + "'>" + this.answers[0].text + "</li><li class = 'answer-choice' data-correct = '" + this.answers[1].dataCorrect + "'>" + this.answers[1].text + "</li><li class = 'answer-choice' data-correct = '" + this.answers[2].dataCorrect + "'>" + this.answers[2].text + "</li><li class = 'answer-choice' data-correct = '" + this.answers[3].dataCorrect + "'>" + this.answers[3].text + "</li></ul><br /> <button class = 'submit'>Submit</button>";
@@ -33,18 +30,42 @@ startButton.addEventListener("click", startQuiz);
 
 function buildQuestionsArr() {
     for (let i = 0; i < rawQuestions.length; i++) {
-        questions.push(new Question(idCounter, rawQuestions[i], rawCode[i], rawAnswers[i]));
+        questions.push(new Question(idCounter, rawQuestions[i], multiSelectQuestion[i], rawCode[i], rawAnswers[i]));
+    }
+}
+
+function toggleSelected(event) {
+    let clickedAnswer = event.target;
+    if (clickedAnswer.classList.contains("selected")) {
+        clickedAnswer.className = "answer-choice";
+    } else {
+        clickedAnswer.className = "answer-choice selected";
+        if (!multiSelect) {
+            let otherAnswer = clickedAnswer.parentNode.firstElementChild;
+            while (otherAnswer) {
+                if (otherAnswer != clickedAnswer) {
+                    otherAnswer.className = "answer-choice";
+                }
+                otherAnswer = otherAnswer.nextElementSibling;
+            }
+        }
     }
 }
 
 function pickQuestion() {
     if (document.getElementsByClassName(".question").length > 0) {
         let prevQuestions = document.getElementsByClassName(".question");
-        for (element in document.getElementsByClassName(".question")) {
+        for (element in prevQuestions) {
             element.style.display = "none";
         }
     }
-    mainSec.appendChild(questions[Math.floor(Math.random() * questions.length)].section);
+    let newQuestion = questions[Math.floor(Math.random() * questions.length)]
+    mainSec.appendChild(newQuestion.section);
+    let answerBoxes = document.querySelectorAll(".answer-choice");
+    answerBoxes.forEach(answer => {
+        answer.addEventListener("click", toggleSelected);
+    })
+    multiSelect = newQuestion.multiSelect;
 }
 
 function startQuiz() {
