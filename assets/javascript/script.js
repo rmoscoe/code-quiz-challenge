413971d5cbd1bf87927cf2a1d40d116f6c482d41
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2022-12-27 16:46:21 -0800

    provide functionality for countdown timer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,7 +10,12 @@ const rawCode = [""];
 const rawAnswers = [[{text:"&ltlink&gt", dataCorrect:"incorrect"}, {text:"&lta&gt", dataCorrect:"incorrect"}, {text:"&ltsrc&gt", dataCorrect:"correct"}, {text:"&lthref&gt", dataCorrect:"incorrect"}]];
 let questions = [];
 let multiSelect = false;
+let secondsRemaining = 600;
+let mins = 10;
+let secs = 0;
+let timer;
 
+//Each question object contains properties for the question and answer choices, as well as inner HTML that includes class attributes.
 class Question {
     constructor(id, question, multiSelect, code, answers) {
         this.id = id;
@@ -28,6 +33,7 @@ class Question {
 
 startButton.addEventListener("click", startQuiz);
 
+//Use an array to store question objects. When the Start button is clicked and each time a question is submitted, use a random number to select another question from the array, then remove that question from the array.
 function buildQuestionsArr() {
     for (let i = 0; i < rawQuestions.length; i++) {
         questions.push(new Question(idCounter, rawQuestions[i], multiSelectQuestion[i], rawCode[i], rawAnswers[i]));
@@ -53,29 +59,52 @@ function toggleSelected(event) {
 }
 
 function pickQuestion() {
-    if (document.getElementsByClassName(".question").length > 0) {
-        let prevQuestions = document.getElementsByClassName(".question");
+    let prevQuestions = document.getElementsByClassName(".question");
+    if (prevQuestions.length > 0) {
         for (element in prevQuestions) {
             element.style.display = "none";
         }
     }
-    let newQuestion = questions[Math.floor(Math.random() * questions.length)]
-    mainSec.appendChild(newQuestion.section);
-    let answerBoxes = document.querySelectorAll(".answer-choice");
-    answerBoxes.forEach(answer => {
+    if (questions.length > 0) {
+        let newQuestion = questions[Math.floor(Math.random() * questions.length)]
+
+        //Dynamically create and add elements using the stored properties of the question objects.
+        mainSec.appendChild(newQuestion.section);
+        let answerBoxes = document.querySelectorAll(".answer-choice");
+        answerBoxes.forEach(answer => {
         answer.addEventListener("click", toggleSelected);
-    })
-    multiSelect = newQuestion.multiSelect;
+        })
+        multiSelect = newQuestion.multiSelect;
+        let submitButton = mainSec.lastElementChild.lastElementChild;
+        submitButton.addEventListener("click", submitQuestion);
+    } else {
+        endQuiz();
+    }
+}
+
+function submitQuestion() {
+
 }
 
 function startQuiz() {
     welcome.style.display = "none";
     buildQuestionsArr();
     pickQuestion();
+    timer = setInterval(countdown, 1000);
 }
 
-//Use an array to store question objects. When the Start button is clicked and each time a question is submitted, use a random number to select another question from the array, then remove that question from the array.
-//Each question object contains properties for the question and answer choices, as well as inner HTML that includes class attributes.
-//Dynamically create and add elements using the stored properties of the question objects.
 //For the timer, use minute and second variables. Update the display every second and calculate the variables based on a single time remaining variable that decrements every second.
+function countdown() {
+    if (secondsRemaining > 0) {
+        secondsRemaining--;
+        mins = Math.floor(secondsRemaining / 60);
+        secs = String(secondsRemaining % 60).padStart(2, "0");
+        document.getElementById("mins").textContent = mins;
+        document.getElementById("secs").textContent = secs;
+    } else {
+        clearInterval(timer);
+        endQuiz();
+    }
+}
+
 //For the scores, remember localStorage.setItem("Key", "Value") and localStorage.getItem("Key"). preventDefault() the Submit button for the initials associated with the score.
